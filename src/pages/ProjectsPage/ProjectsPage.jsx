6902b36863e4878005dc3c4d3ff5b69c6dbc156a
nazaraliev-kubanychbek <kuba.duishobaevich@gmail.com>
2024-07-28 20:08:46 +0600

    add projectPage and exhibitionPage

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,9 +1,20 @@
+import "./projectsPage.scss";
+import ProjectsPageHero from "./ProjectsPageHero";
+import ProjectsPageProjects from "./ProjectsPageProjects";
+import ProjectsPageInfo from "./ProjectsPageInfo";
 
 export const ProjectsPage = () => {
-    return (
-        <div style={{height: '100vh', backgroundColor: '#051650'}}>
-            <h2>Бизнес проекты</h2>
-        </div>
-    );
-}
-
+  return (
+    <div
+      style={{ minHeight: "100vh", backgroundColor: "#051650" }}
+      className="projectsPage"
+    >
+      <div className="container">
+        <ProjectsPageHero />
+        <ProjectsPageProjects />
+        <button className="projectsPage-btn">Подать заявку</button>
+        <ProjectsPageInfo />
+      </div>
+    </div>
+  );
+};
