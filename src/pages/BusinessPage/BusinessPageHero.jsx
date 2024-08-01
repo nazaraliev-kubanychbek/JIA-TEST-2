9a4454a37d7a6712c51409ff1e7abc0ac59d2913
nazaraliev-kubanychbek <kuba.duishobaevich@gmail.com>
2024-08-01 15:13:31 +0600

    kuba-task3

--- src/pages/BusinessPage/BusinessPageHero.jsx
+++ src/pages/BusinessPage/BusinessPageHero.jsx
@@ -1,12 +1,18 @@
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const BusinessPageHero = () => {
+  const [text, setText] = useState({});
+
+  useEffect(()=>{
+    axios('https://bif.webtm.ru/ru/api/v1/project/projects/')
+    .then(({data})=> setText(data[0]))
+  }, [])
     return (
         <div className="businessPage-hero">
-        <h1 className="businessPage-hero-title">Бизнес проекты</h1>
-        <p className="businessPage-hero-text">Готовы привлечь инвестиции для вашего бизнес-проекта? Участвуйте в инвестиционном форуме BIF 2024 и представьте свой бизнес перед ведущими инвесторами. Получите шанс на финансирование до 4,5 миллионов долларов!</p>
-        <p className="businessPage-hero-text">В этом году будет отобраны 100 бизнес-проектов для размещения в каталоге BIF 2024, из которых 15 лучших смогут выступить на форуме с презентацией.</p>
-      </div>
+        <h1 className="businessPage-hero-title">{text.title}</h1>
+        <p className="businessPage-hero-text">{text.descriptons}</p>
+        </div>
     );
 }
 
--- src/pages/BusinessPage/BusinessProjects.jsx
+++ src/pages/BusinessPage/BusinessProjects.jsx
@@ -1,42 +1,25 @@
-import img1 from './img/project1.png';
-import img2 from './img/project2.png';
-import img3 from './img/project3.png';
-import img4 from './img/project4.png';
-import img5 from './img/project5.png';
 
-const projectList = [
-    {
-        img: img1,
-        text: '2015',
-    },
-    {
-        img: img2,
-        text: '2016',
-    },
-    {
-        img: img3,
-        text: '2017',
-    },
-    {
-        img: img4,
-        text: '2019',
-    },
-    {
-        img: img5,
-        text: '2022',
-    },
-]
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const BusinessProjects = () => {
+    const [projectList, setProjectList] = useState([]);
+
+    useEffect(()=>{
+        axios('https://bif.webtm.ru/ru/api/v1/project/yearly-catalogs/')
+        .then(({data}) => setProjectList(data));
+    }, [])
     return (
         <div className="businessPage-projects">
+            <div className="businessPage-projects-rightLine"></div>
         <div className="row">
                     {
                         projectList.map(item =>{
-                            return <div key={item.text} className="col-4">
+                            return <div key={item.id} className="col-4">
                                 <div className="businessPage-projects-card">
-                                    <p className="businessPage-projects-card-text">{item.text}</p>
-                                    <img src={item.img} alt="" className="businessPage-projects-card-img" />
+                                    <p className="businessPage-projects-card-text">{item.year}</p>
+                                    <img src={item.image} alt="" className="businessPage-projects-card-img" />
                                 </div>
                             </div>
                         })
